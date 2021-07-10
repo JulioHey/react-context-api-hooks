dd890eaff5bb3a7de51252b220010b45f3c22341
Julio Hey <juliohey.dev@gmail.com>
2021-07-10 19:36:54 -0300

    Projeto utilizando os conceitos de reducer + contexts

---
+++ src/contexts/CounterContext/reducer.js
@@ -0,0 +1,30 @@
+import { initialState } from '.';
+import * as actionsTypes from './actions-types';
+
+export const counterReducer = (state, action) => {
+  console.log(action.type);
+
+  switch (action.type) {
+    case actionsTypes.INCREASE: {
+      return { ...state, counter: state.counter + 1 };
+    }
+    case actionsTypes.DECREASE: {
+      return { ...state, counter: state.counter - 1 };
+    }
+    case actionsTypes.RESET: {
+      return { ...initialState };
+    }
+    case actionsTypes.SET_COUNTER: {
+      return { ...state, ...action.payload };
+    }
+    case actionsTypes.ASYNC_INCREASE_START: {
+      return { ...state, loading: true };
+    }
+    case actionsTypes.ASYNC_INCREASE_END: {
+      return { ...state, loading: false, counter: state.counter + 1 };
+    }
+    case actionsTypes.ASYNC_INCREASE_ERROR: {
+      return { ...state, loading: false };
+    }
+  }
+};
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,6 +1,35 @@
+import { useEffect, useState } from 'react';
 import { useCounterContext } from '../../contexts/CounterContext';
 
+import { Button } from '../../components/Button';
+import { Heading } from '../../components/Heading';
+
 export const Home = () => {
-  const [state, dispatch] = useCounterContext();
-  return <h1>Oi</h1>;
+  const [state, actions] = useCounterContext();
+
+  const handleError = async () => {
+    actions
+      .asyncError()
+      .then((r) => console.log(r))
+      .catch((e) => console.log(e.name, ':', e.message));
+  };
+
+  return (
+    <div>
+      <Heading>{state.counter}</Heading>;
+      <div>
+        <Button onButtonClick={actions.increase}>Increase</Button>
+        <Button onButtonClick={actions.decrease}>Decrease</Button>
+        <Button onButtonClick={actions.reset}>Reset</Button>
+        <Button onButtonClick={() => actions.setCounter({ counter: 10 })}>set10</Button>
+        <Button onButtonClick={() => actions.setCounter({ counter: 100 })}>set100</Button>
+        <Button disabled={state.loading} onButtonClick={actions.asyncIncrease}>
+          AsyncIncrease
+        </Button>
+        <Button disabled={state.loading} onButtonClick={handleError}>
+          AsyncError
+        </Button>
+      </div>
+    </div>
+  );
 };
